df6de719a5c75c1a0a558063bd819458f3a3cc7c
sampopulous <65770070+sampopulous@users.noreply.github.com>
2020-09-19 13:09:53 +0530

    upload-app

---
+++ www/js/config.js
@@ -0,0 +1,9 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={			
+	'ApiUrl':"https://overbowl.in/dev/driver/api",		
+	'DialogDefaultTitle':"Delivery Boy App",
+	'mapboxToken' : '',
+	'APIHasKey':"kdnsaohdnslkdjsgdksdjdjSDddsgh",
+	'debug': false
+};
