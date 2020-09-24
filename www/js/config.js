69efd542c9b43f32a24c21683c353e5285d31309
sampopulous <65770070+sampopulous@users.noreply.github.com>
2020-09-24 15:46:59 +0530

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 /*Driver App Configuration*/
 
 var krms_driver_config ={			
-	'ApiUrl':"https://overbowl.in/dev/driver/api",		
+	'ApiUrl':"https://overbowl.in/driver/api",		
 	'DialogDefaultTitle':"Delivery Boy App",
 	'mapboxToken' : '',
 	'APIHasKey':"kdnsaohdnslkdjsgdksdjdjSDddsgh",
